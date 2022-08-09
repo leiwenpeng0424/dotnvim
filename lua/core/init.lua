08406d6d92a7192f5a3671d296cf8fd030e6bb7d
wenpeng.lei <wenpeng.lei@ncsi.com.cn>
2022-08-09 16:12:09 +0800

    add new plugins

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,14 +1,10 @@
 local pack = require('core.pack')
 
 function load()
-
     require('core.options')
-
     pack.ensure_packer_installed()
     pack.load_compile()
-
     require('core.keymap')
-
     vim.cmd([[set background=dark]])
     vim.cmd[[colorscheme terafox]]
 end
--- lua/plugins/tools/neo-tree.lua
+++ lua/plugins/tools/neo-tree.lua
@@ -14,7 +14,7 @@ use({
         require('neo-tree').setup({
             close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
             popup_border_style = "rounded",
-            enable_git_status = false,
+            enable_git_status = true,
             enable_diagnostics = false,
             default_component_configs = {
                 container = {
