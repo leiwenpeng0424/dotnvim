7f048b01d587699d4ce4d1a697d600e895813077
leiwenpeng <leiwenpeng1994@outlook.com>
2022-08-23 20:03:46 +0800

    chore: minor upgration

---
+++ lua/plugins/ui/github-nvim-theme.lua
@@ -0,0 +1,10 @@
+local use = require('packer').use
+
+use({
+    'projekt0n/github-nvim-theme',
+    config = function()
+        require('github-theme').setup({
+            dark_float = true,
+        })
+    end
+})
