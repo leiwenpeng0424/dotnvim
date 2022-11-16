210f7395ce76cbd240589089aa7b6acb711a8911
wenpeng.lei <wenpeng.lei@ncsi.com.cn>
2022-11-16 17:33:04 +0800

    add

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -5,72 +5,72 @@ local mappings = {
     [';|:'] = { mode = 'n', opts = { noremap = true } },
 
     -- save with ctrl + s, V/I mode
-    ['<c-s>|:w<cr>']                             = { mode = '', opts = { noremap = true } },
-    ['<c-s>|<C-o>:w<cr>']                        = { mode = 'i', opts = { noremap = true } },
-    ['<Leader>s|:w<cr>']                         = { mode = 'n', opts = { noremap = true } },
+    ['<c-s>|:w<cr>']      = { mode = '', opts = { noremap = true } },
+    ['<c-s>|<C-o>:w<cr>'] = { mode = 'i', opts = { noremap = true } },
+    ['<Leader>s|:w<cr>']  = { mode = 'n', opts = { noremap = true } },
 
     -- open vertical split
-    ['<leader>v|<c-w>v']                         = { mode = 'n', opts = { noremap = true } },
+    ['<leader>v|<c-w>v'] = { mode = 'n', opts = { noremap = true } },
 
     -- down is really the next line
-    ['j|gj']                                     = { mode = 'n', opts = { noremap = true } },
-    ['k|gk']                                     = { mode = 'n', opts = { noremap = true } },
+    ['j|gj'] = { mode = 'n', opts = { noremap = true } },
+    ['k|gk'] = { mode = 'n', opts = { noremap = true } },
 
     -- copy to system clipboard
-    ['<c-c>|"+y']                                = { mode = 'n', opts = { noremap = true } },
+    ['<c-c>|"+y'] = { mode = 'n', opts = { noremap = true } },
 
     -- paste from system clipboard with ctrl + v
-    ['<c-v>|<Esc>"+p']                           = { mode = 'i', opts = { noremap = true } },
-    ['<leader-p>|"0p']                           = { mode = 'n', opts = { noremap = true } },
-    ['<leader-p>|"0p']                           = { mode = 'v', opts = { noremap = true } },
-    ['<leader-h>|viw"0p']                        = { mode = 'n', opts = { noremap = true, nowait = false } },
+    ['<c-v>|<Esc>"+p']    = { mode = 'i', opts = { noremap = true } },
+    ['<leader-p>|"0p']    = { mode = 'n', opts = { noremap = true } },
+    ['<leader-p>|"0p']    = { mode = 'v', opts = { noremap = true } },
+    ['<leader-h>|viw"0p'] = { mode = 'n', opts = { noremap = true, nowait = false } },
 
     -- move to the end of yanked text after yank and pasted
-    ['p|p`]']                                    = { mode = 'n', opts = { noremap = true } },
-    ['p|p`]']                                    = { mode = 'v', opts = { noremap = true } },
-    ['y|y`]']                                    = { mode = 'v', opts = { noremap = true } },
+    ['p|p`]'] = { mode = 'n', opts = { noremap = true } },
+    ['p|p`]'] = { mode = 'v', opts = { noremap = true } },
+    ['y|y`]'] = { mode = 'v', opts = { noremap = true } },
 
     -- Move selected lines up and down
-    ["<c-j>|:m '>+1<cr>gv=gv"]                   = { mode = 'v', opts = { noremap = true } },
-    ["<c-k>|:m '<-2<cr>gv=gv"]                   = { mode = 'v', opts = { noremap = true } },
-    ["_|<c-w>5<"]                                = { mode = 'n', opts = { noremap = true } },
-    ['+|<c-w>5>']                                = { mode = 'n', opts = { noremap = true } },
-    ['{|:resize -5<cr>']                    = { mode = 'n', opts = { noremap = true } },
-    ['}|:resize +5<cr>']                    = { mode = 'n', opts = { noremap = true } },
-    ['<c-b>|<cmd>NeoTreeShow<cr>']         = { mode = 'n', opts = { noremap = true } },
-    ['<leader>b|<cmd>Neotree<cr>']         = { mode = 'n', opts = { noremap = true } },
+    ["<c-j>|:m '>+1<cr>gv=gv"]     = { mode = 'v', opts = { noremap = true } },
+    ["<c-k>|:m '<-2<cr>gv=gv"]     = { mode = 'v', opts = { noremap = true } },
+    ["_|<c-w>5<"]                  = { mode = 'n', opts = { noremap = true } },
+    ['+|<c-w>5>']                  = { mode = 'n', opts = { noremap = true } },
+    ['{|:resize -5<cr>']           = { mode = 'n', opts = { noremap = true } },
+    ['}|:resize +5<cr>']           = { mode = 'n', opts = { noremap = true } },
+    ['<c-b>|<cmd>NeoTreeShow<cr>'] = { mode = 'n', opts = { noremap = true } },
+    ['<leader>b|<cmd>Neotree<cr>'] = { mode = 'n', opts = { noremap = true } },
 
     -- lspsaga
-    ['gf|<cmd>Lspsaga lsp_finder<cr>']           = { mode = 'n', opts = { noremap = true } },
-    ['gs|<cmd>Lspsaga signature_help<cr>']       = { mode = 'n', opts = { noremap = true } },
-    ['d[|<cmd>Lspsaga diagnostic_jump_prev<cr>'] = { mode = 'n', opts = { noremap = true } },
-    ['d]|<cmd>Lspsaga diagnostic_jump_next<cr>'] = { mode = 'n', opts = { noremap = true } },
-    ['gr|<cmd>Lspsaga rename<cr>']               = { mode = 'n', opts = { noremap = true } },
-    ['<leader>ca|<cmd>Lspsaga code_action<cr>']          = { mode = 'n', opts = { noremap = true } },
-    ['gc|<cmd>Lspsaga range_code_action<cr>']    = { mode = 'n', opts = { noremap = true } },
-    ['gd|<cmd>Lspsaga peek_definition<cr>']   = { mode = 'n', opts = { noremap = true } },
-    ['K|<cmd>Lspsaga hover_doc<cr>']             = { mode = 'n', opts = { noremap = true } },
-    ["<F2>|<cmd>Lspsaga rename<cr>"] = { mode = 'n', opts = { noremap = true } },
+    ['gf|<cmd>Lspsaga lsp_finder<cr>']                                = { mode = 'n', opts = { noremap = true } },
+    ['gs|<cmd>Lspsaga signature_help<cr>']                            = { mode = 'n', opts = { noremap = true } },
+    ['d[|<cmd>Lspsaga diagnostic_jump_prev<cr>']                      = { mode = 'n', opts = { noremap = true } },
+    ['d]|<cmd>Lspsaga diagnostic_jump_next<cr>']                      = { mode = 'n', opts = { noremap = true } },
+    ['gr|<cmd>Lspsaga rename<cr>']                                    = { mode = 'n', opts = { noremap = true } },
+    ['<leader>ca|<cmd>Lspsaga code_action<cr>']                       = { mode = 'n', opts = { noremap = true } },
+    ['gc|<cmd>Lspsaga range_code_action<cr>']                         = { mode = 'n', opts = { noremap = true } },
+    ['gd|<cmd>Lspsaga peek_definition<cr>']                           = { mode = 'n', opts = { noremap = true } },
+    ['K|<cmd>Lspsaga hover_doc<cr>']                                  = { mode = 'n', opts = { noremap = true } },
+    ["<F2>|<cmd>Lspsaga rename<cr>"]                                  = { mode = 'n', opts = { noremap = true } },
     ["<leader>fm|<cmd>:lua vim.lsp.buf.format({ async = true })<cr>"] = { mode = 'n' },
 
     -- move cursor
-    ['<leader><Down>|<c-w>j']                    = { mode = 'n', opts = { noremap = true } },
-    ['<leader><Left>|<c-w>h']                    = { mode = 'n', opts = { noremap = true } },
-    ['<leader><Right>|<c-w>l']                   = { mode = 'n', opts = { noremap = true } },
-    ['<leader><Up>|<c-w>k']                      = { mode = 'n', opts = { noremap = true } },
+    ['<leader><Down>|<c-w>j']  = { mode = 'n', opts = { noremap = true } },
+    ['<leader><Left>|<c-w>h']  = { mode = 'n', opts = { noremap = true } },
+    ['<leader><Right>|<c-w>l'] = { mode = 'n', opts = { noremap = true } },
+    ['<leader><Up>|<c-w>k']    = { mode = 'n', opts = { noremap = true } },
 
     -- telescope
     ['<leader>ff|<cmd>Telescope find_files<cr>'] = { mode = 'n', opts = { noremap = true } },
     ['<leader>fb|<cmd>Telescope buffers<cr>']    = { mode = 'n', opts = { noremap = true } },
 
     -- barbar
-    ['<S-TAB>|<cmd>BufferPrevious<cr>']               = { mode = 'n', opts = { noremap = true } },
-    ['<TAB>|<cmd>BufferNext<cr>']                   = { mode = 'n', opts = { noremap = true } },
-    ['bp|<cmd>BufferPin<cr>']                    = { mode = 'n', opts = { noremap = true } },
-    ['<leader>x|<cmd>BufferClose<cr>']                  = { mode = 'n', opts = { noremap = true } },
-    ['b<|<cmd>BufferMovePrevious<cr>']           = { mode = 'n', opts = { noremap = true } },
-    ['b>|<cmd>BufferMoveNext<cr>']               = { mode = 'n', opts = { noremap = true } },
-    ['bP|<cmd>BufferPick<cr>']                   = { mode = 'n', opts = { noremap = false } },
+    ['<S-TAB>|<cmd>BufferPrevious<cr>'] = { mode = 'n', opts = { noremap = true } },
+    ['<TAB>|<cmd>BufferNext<cr>']       = { mode = 'n', opts = { noremap = true } },
+    ['bp|<cmd>BufferPin<cr>']           = { mode = 'n', opts = { noremap = true } },
+    ['<leader>x|<cmd>BufferClose<cr>']  = { mode = 'n', opts = { noremap = true } },
+    ['b<|<cmd>BufferMovePrevious<cr>']  = { mode = 'n', opts = { noremap = true } },
+    ['b>|<cmd>BufferMoveNext<cr>']      = { mode = 'n', opts = { noremap = true } },
+    ['bP|<cmd>BufferPick<cr>']          = { mode = 'n', opts = { noremap = false } },
 
     -- nvim-terminal
     ['<leader>t|<cmd>:lua NTGlobal["terminal"]:toggle()<cr>'] = { mode = 'n', opts = { noremap = true } },
